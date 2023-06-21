f19b57e2b8dd631fe17292bd8c765b768c4e93a8
Vinicius Evair <vsilva.evair@gmail.com>
2023-06-20 23:49:45 -0300

    Fixed search function not returning a pointer/null

--- trab2/libHASH.c
+++ trab2/libHASH.c
@@ -11,21 +11,38 @@ struct tabela_hash_t* cria_tabela() {
         fprintf(stderr, "Falha ao alocar memória.\n");
         exit(EXIT_FAILURE);
     }
+
+    /*
+     * Guarda o dobro de HASHTAM para emular duas tabelas, utilizando um único
+     * vetor.
+     */
     temp->tam = HASHTAM * 2;
 
     if(! (temp->T = calloc(temp->tam, sizeof(struct pos_hash_t*)))) {
         fprintf(stderr, "Falha ao alocar memória.\n");
         exit(EXIT_FAILURE);
     }
+    for (int i = 0; i < temp->tam; i++)
+        if(! (temp->T[i] = malloc(sizeof(struct pos_hash_t)))) {
+            fprintf(stderr, "Falha ao alocar memória.\n");
+            exit(EXIT_FAILURE);
+        }
 
+    /*
+     * Inicializa as posições da tabela, guardando o estado vazio e a posição na
+     * estrutura (para facilitar a impressão depois).
+     */
     for (int i = 0; i < temp->tam; i++) {
-        temp->T[i].estado = VAZIO;
+        temp->T[i]->estado = VAZIO;
+        temp->T[i]->pos = i;
     }
 
     return temp;
 }
 
 void destroi_tabela(struct tabela_hash_t *tabela) {
+    for (int i = 0; i < tabela->tam; i++)
+        free(tabela->T[i]);
     free(tabela->T);
     free(tabela);
 }
@@ -47,48 +64,54 @@ void insere_valor(struct tabela_hash_t *tabela, int chave) {
     int pos_t1, pos_t2;
     pos_t1 = h1(chave);
 
-    if(tabela->T[pos_t1].estado != OCUPADO) {
-        tabela->T[pos_t1].chave = chave;
-        tabela->T[pos_t1].estado = OCUPADO;
+    if(tabela->T[pos_t1]->estado != OCUPADO) {
+        tabela->T[pos_t1]->chave = chave;
+        tabela->T[pos_t1]->estado = OCUPADO;
     } else {
-        pos_t2 = h2(tabela->T[pos_t1].chave);
-        tabela->T[pos_t2].chave = tabela->T[pos_t1].chave;
-        tabela->T[pos_t2].estado = OCUPADO;
-        tabela->T[pos_t1].chave = chave;
-        tabela->T[pos_t1].estado = OCUPADO;
+        if(tabela->T[pos_t1]->chave == chave)
+            return;
+        pos_t2 = h2(tabela->T[pos_t1]->chave);
+        tabela->T[pos_t2]->chave = tabela->T[pos_t1]->chave;
+        tabela->T[pos_t2]->estado = OCUPADO;
+        tabela->T[pos_t1]->chave = chave;
+        tabela->T[pos_t1]->estado = OCUPADO;
     }
 }
 
-int busca_valor(struct tabela_hash_t *tabela, int chave) {
+struct pos_hash_t *busca_valor(struct tabela_hash_t *tabela, int chave) {
     int pos_t1, pos_t2;
     pos_t1 = h1(chave);
 
-    if(tabela->T[pos_t1].chave == chave) {
-        return pos_t1;
-    } else if (tabela->T[pos_t1].estado != VAZIO) {
+    if(tabela->T[pos_t1]->chave == chave) {
+        return tabela->T[pos_t1];
+    } else if (tabela->T[pos_t1]->estado != VAZIO) {
         pos_t2 = h2(chave);
-        if(tabela->T[pos_t2].chave == chave)
-            return pos_t2;
+        if(tabela->T[pos_t2]->chave == chave)
+            return tabela->T[pos_t2];
     }
 
-    return -1;
+    return NULL;
 }
 
 void remove_valor(struct tabela_hash_t *tabela, int chave) {
     int pos_t1, pos_t2;
     pos_t1 = h1(chave);
 
-    if(tabela->T[pos_t1].chave == chave) {
+    if(tabela->T[pos_t1]->chave == chave) {
         //arrumar os ponteiros depois
-        tabela->T[pos_t1].estado = EXCLUIDO;
+        tabela->T[pos_t1]->estado = EXCLUIDO;
     } else {
         pos_t2 = h2(chave);
-        if(tabela->T[pos_t2].chave == chave) {
-            tabela->T[pos_t2].estado = EXCLUIDO;
+        if(tabela->T[pos_t2]->chave == chave) {
+            tabela->T[pos_t2]->estado = EXCLUIDO;
         }
     }
 }
 
+/*
+ * Função simples que troca os valores guardados nos indices a e b do vetor.
+ * Função auxiliar utilizada pelo Insertion Sort.
+ */
 void trocar(int vetor[], int a, int b) {
     int aux;
     aux = vetor[a];
@@ -96,6 +119,7 @@ void trocar(int vetor[], int a, int b) {
     vetor[b] = aux;
 }
 
+// Busca binária. Função auxiliar utilizada pelo Insertion Sort.
 int buscar(int valor, int vetor[], int ini, int fim) {
     int meio;
 
@@ -113,11 +137,11 @@ int buscar(int valor, int vetor[], int ini, int fim) {
 
 /*
  * Recebe um vetor indexado por [0..tam-1] com todos seus valores ordenados com
- * excessao do ultimo.
- * Ordena o vetor de maneira que fique completamente ordenado.
- * Retorna a quantidade de comparacoes feitas durante o processo.
+ * excessao do ultimo. Corrige a posição do último valor no vetor de maneira com
+ * que o vetor fique completamente ordenado.
+ * Função auxiliar utilizada pelo Insertion Sort.
  */ 
-void insere(int vetor[], int tam) {
+void ordena(int vetor[], int tam) {
     int posicao, i, ini, fim;
     ini = 0;
     fim = tam - 1;
@@ -128,41 +152,51 @@ void insere(int vetor[], int tam) {
 }
 
 /*
- * Recebe um vetor indexado por [0..tam-1].
- * Ordena o vetor recebido.
- * Retorna a quantidade de comparacoes feitas no processo.
+ * Recebe um vetor indexado por [0..tam-1]. Ordena o vetor recebido.
+ * Função auxiliar utilizada pelo Insertion Sort.
  */
 void insertionSort(int vetor[], int tam){	
     if(tam <= 1)
         return;
 
     insertionSort(vetor, tam - 1);
-    insere(vetor, tam);
+    ordena(vetor, tam);
 }
 
 void escreve_ordenado(struct tabela_hash_t *tabela) {
-    int pos_chave, count;
+    int count;
     int *aux;
     if(! (aux = calloc(HASHTAM * 2, sizeof(int)))) {
         fprintf(stderr, "Erro ao alocar memória");
         exit(EXIT_FAILURE);
     }
 
+    /*
+     * Adiciona as posições que estejam ocupadas com alguma chave da tabela no
+     * vetor a ser ordenado.
+     */
     count = 0;
     for (int i = 0; i < tabela->tam; i++) {
-        if(tabela->T[i].estado == OCUPADO) {
-            aux[count] = tabela->T[i].chave;
+        if(tabela->T[i]->estado == OCUPADO) {
+            aux[count] = tabela->T[i]->chave;
             count++;
         }
     }
 
+    /*
+     * Ordena o vetor utilizando o Insertion Sort, que é estável e portanto
+     * garante que, no caso de ambos T1 e T2 conterem uma mesma chave, o dado de
+     * T1 vai continuar antes do dado de T2 após a ordenação. A escolha de um
+     * ordenador estável acaba sendo simbólica, uma vez que nesta implementação,
+     * chaves repetidas não ocorrem.
+     */
     insertionSort(aux, count);
 
     for (int i = 0; i < count; i++) {
-        pos_chave = busca_valor(tabela, aux[i]);
-        if(pos_chave < 11)
-            printf("%d,T1,%d\n", aux[i], pos_chave);
+        struct pos_hash_t *ponteiro_aux = busca_valor(tabela, aux[i]);
+        if(ponteiro_aux->pos < 11)
+            printf("%d,T1,%d\n", aux[i], ponteiro_aux->pos);
         else
-            printf("%d,T2,%d\n", aux[i], pos_chave % HASHTAM);
+            printf("%d,T2,%d\n", aux[i], ponteiro_aux->pos % HASHTAM);
     }
 }
--- trab2/libHASH.h
+++ trab2/libHASH.h
@@ -1,36 +1,91 @@
+/*
+ * Biblioteca implementada pelo aluno Vinicius Evair da Silva para o Trabalho
+ * Pratico 2, da disciplina de Algoritmos e Estrutura de Dados III (CI1057).
+ */
+
 #ifndef __HASH
 #define __HASH
 
+/*
+ * Controle de estado das posições da HASH. O estado EXCLUIDO é necessário para
+ * acessar a segunda tabela de dados.
+ */
 enum estado_t {
     VAZIO,
     OCUPADO,
     EXCLUIDO,
 };
 
+/*
+ * Estrutura de posições da hash. Guarda uma chave, o estado da posição e
+ * possívelmente um dado. Em especial, no caso deste trabalho, não há a
+ * necessidade de guardar nenhum dado.
+ */
 struct pos_hash_t {
     int chave;
     enum estado_t estado;
+    int pos;
 };
 
+/*
+ * Estrutura da tabela hash, que guarda o vetor da tabela e o tamanho. O vetor
+ * guarda o dobro de "tam", emulando assim duas tabelas (T1 e T2), mas guardadas
+ * de maneira contigua na memória.
+ */
 struct tabela_hash_t {
-    struct pos_hash_t *T;
+    struct pos_hash_t **T;
     int tam;
 };
 
+// Inicializa a tabela, alocando memória para o vetor.
 struct tabela_hash_t* cria_tabela();
 
+// Libera toda a memória utilizada pela tabela.
 void destroi_tabela(struct tabela_hash_t *tabela);
 
+/*
+ * Função hash para as primeiras onze posições da tabela (tabela T1). Utiliza o
+ * resto da divisão do valor da chave por HASHTAM. Em caso de chave negativa,
+ * devolve o resto POSITIVO da divisão por HASHTAM (i. e., soma-se tam ao
+ * resultado).
+ */
 int h1(int chave);
 
+/*
+ * Função hash para as onze últimas posições da tabela (tabela T2).
+ * Utiliza a expressão ⌊tam * (chave * 0.9 − ⌊chave * 0.9⌋)⌋ para calcular a
+ * posição. Soma-se 11 ao resultado para utilizar as posições reservadas para
+ * T2.
+ */
 int h2(int chave);
 
+/*
+ * Adiciona um novo valor na tabela hash. Nesta implementação, não são possíveis
+ * chaves repetidas. Em caso de repetição das chaves, a chave mais nova é
+ * descartada.
+ */
 void insere_valor(struct tabela_hash_t *tabela, int chave);
 
-int busca_valor(struct tabela_hash_t *tabela, int chave);
+/*
+ * Busca um valor na tabela. A busca pela chave é realizada primeiramente na T1
+ * e, em caso de falha, busca na T2. Retorna a posição da chave em caso de
+ * sucesso e retorna -1 em caso de falha. Se o valor de retorno positivo for
+ * menor que onze, o dado se encontra na T1, caso contrário, o dado está na T2.
+ */
+struct pos_hash_t *busca_valor(struct tabela_hash_t *tabela, int chave);
 
+/*
+ * Remove um valor da tabela hash, liberando a posição.
+ */
 void remove_valor(struct tabela_hash_t *tabela, int chave);
 
+/*
+ * Imprime os valores guardados na tabela de maneira ordenada. A ordem da
+ * impressão é crescente e de acordo com o valor da chave. Apesar do fato de que
+ * nesta implementação não são aceitas chaves repetidas, ainda assim a função
+ * garante que a hierarquia de ordenação em caso de repetições seja a chave,
+ * seguido de qual tabela o dado está e por último a posição na tabela.
+ */
 void escreve_ordenado(struct tabela_hash_t *tabela);
 
 #endif
